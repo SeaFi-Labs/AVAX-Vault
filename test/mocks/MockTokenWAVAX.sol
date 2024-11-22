200170e983ef95642f376335cd9fca46a33800f8
bookland <RusseII@users.noreply.github.com>
2024-11-22 07:33:46 -0600

    update

--- test/mocks/MockTokenWAVAX.sol
+++ test/mocks/MockTokenWAVAX.sol
@@ -1,42 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "forge-std/console.sol";
 
-// [GGP] use the real WAVAX instead of this when deploying to prod
-
-contract MockTokenWAVAX is ERC20("Wrapped AVAX", "AVAX") {
-    using SafeERC20 for IERC20;
-
+contract MockTokenWAVAX is ERC20 {
     event Deposit(address indexed from, uint256 amount);
-
     event Withdrawal(address indexed to, uint256 amount);
 
-    constructor(address initialRecipient) {
-        // Mint 18 million tokens (18,000,000 * 10^18 for 18 decimals)
+    constructor(address initialRecipient) ERC20("Wrapped Native", "WNATIVE") {
         _mint(initialRecipient, 18_000_000 * 10 ** decimals());
     }
 
-    function deposit() public payable virtual {
-        console.log("wavax ran", msg.sender, msg.value);
+    function deposit() external payable {
         _mint(msg.sender, msg.value);
-        console.log("wavax ran2", msg.sender, msg.value);
-
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 amount) public virtual {
-        _burn(msg.sender, amount);
+    function withdraw(uint256 _amount) external {
+        _burn(msg.sender, _amount);
+        emit Withdrawal(msg.sender, _amount);
 
-        emit Withdrawal(msg.sender, amount);
-
-        // msg.sender.safeTransfer(amount);
-    }
-
-    receive() external payable virtual {
-        deposit();
+        (bool success,) = msg.sender.call{value: _amount}("");
+        if (!success) {
+            revert("Withdraw failed");
+        }
     }
 }
