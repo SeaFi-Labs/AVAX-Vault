e3bb51a948dbc20bc78e69fe0eb98e0e1f6e3a17
bookland <rratcliffe57@gmail.com>
2024-11-22 09:18:48 -0600

    rename and more tests

--- test/mocks/WAVAXVaultV2.sol
+++ test/mocks/WAVAXVaultV2.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.20;
 
 import {WAVAXVault} from "../../contracts/WAVAXVault.sol";
 
-/// @custom:oz-upgrades-from GGPVault
-contract AVAXVaultV2 is WAVAXVault {
+/// @custom:oz-upgrades-from WAVAXVault
+contract WAVAXVaultV2 is WAVAXVault {
     function newMethod() public pure returns (string memory) {
         return "meow";
     }
